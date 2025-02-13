1c2441f1171fc0d2a59216245e7854aab5a3a2d2
Tanryberdi <tanryberdi@gmail.com>
2025-02-13 10:48:30 +0500

    removed commented codes

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -11,27 +11,6 @@ import (
 	"github.com/faiface/pixel/pixelgl"
 )
 
-//
-//func run() {
-//	// Parse command line flags
-//	serverAddr := flag.String("server", "localhost:8080", "server address")
-//	flag.Parse()
-//
-//	// Create and start client
-//	gameClient, err := client.NewGameClient(*serverAddr)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	if err := gameClient.Start(); err != nil {
-//		log.Fatal(err)
-//	}
-//}
-//
-//func main() {
-//	pixelgl.Run(run)
-//}
-
 func init() {
 	// This is needed for macOS
 	runtime.LockOSThread()
